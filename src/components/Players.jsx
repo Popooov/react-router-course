02b4f92e5a93cb1943de15b81dd02d6b1297301e
Oleksandr Popov <31711254+Popooov@users.noreply.github.com>
2022-12-07 17:09:32 +0000

    Create Teampage, Players components and utils func

--- src/components/Players.jsx
+++ src/components/Players.jsx
@@ -1,5 +1,13 @@
+import { useLocation, useSearchParams } from 'react-router-dom'
+
 export default function Players () {
+    const location = useLocation()
+    const [ searchParams ] = useSearchParams(location.search)
+    const team = searchParams.get('teamId')
+
     return (
-        <div className="container">Players</div>
+        <div className="container">
+            Players for team {team}
+        </div>
     )
 }
